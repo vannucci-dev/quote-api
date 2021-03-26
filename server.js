67548ce22f470ae76997533f40995c2d1ecee28a
Lorenzo Vannucci <info@vannucci-dev.com>
2021-03-26 21:21:19 +0000

    create get and push methods

--- server.js
+++ server.js
@@ -1,10 +1,45 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const { quotes } = require('./data');
-const { getRandomElement } = require('./utils');
+const { quotes } = require("./data");
+const { getRandomElement } = require("./utils");
 
 const PORT = process.env.PORT || 4001;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
+app.get("/api/quotes", (req, res, next) => {
+  if (req.query.person) {
+    const quotesByPerson = quotes.filter(
+      (quote) => quote.person === req.query.person
+    );
+    if (quotesByPerson.length > 0) {
+      res.send({ quotes: quotesByPerson });
+    } else {
+      res.send([]);
+    }
+  } else {
+    res.send({ quotes: quotes });
+  }
+});
+
+app.get("/api/quotes/random", (req, res, next) => {
+  res.send({ quote: getRandomElement(quotes) });
+});
+
+app.post("/api/quotes", (req, res, next) => {
+  const newQuote = req.query.quote;
+  const newPerson = req.query.person;
+
+  if (newQuote && newPerson) {
+    const quote = req.query;
+    quotes.push(quote);
+    res.status(201).send({ quote: quote });
+  } else {
+    res.status(400).send();
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started at port ${PORT}`);
+});
